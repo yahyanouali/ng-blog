b4369c363a42fb17c2229b0ea76d0f8ec4305594
Yahya NOUALI <y.nouali@gmail.com>
2020-05-01 16:34:46 +0200

    Added First Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { AboutComponent } from './about/about.component';
 
+const routes: Routes = [
+  { path: 'articles', component: ArticleListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: ArticleListComponent }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
